bfb2c519073a9693cd81fe34acdcff96de9f69f1
Koray Latif <i.koray@me.com>
2025-09-27 21:22:06 -0400

    Documentation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 
 #include "common/SerialHandler.hpp"
 
-
 struct test_struct {
     int32_t a;
     int32_t b;
@@ -18,7 +17,7 @@ int main() {
 
     serial_handler.structs_to_packet_ids.emplace(std::type_index(typeid(test_struct)), PacketId::Hello);
     serial_handler.handlers.emplace(PacketId::Hello, [](const uint8_t* data) {
-        const test_struct* test = reinterpret_cast<const test_struct*>(data);
+        const auto test = reinterpret_cast<const test_struct*>(data);
         printf("Received packet with a: %d, b: %d, c: %d\n", test->a, test->b, test->c);
     });
 
