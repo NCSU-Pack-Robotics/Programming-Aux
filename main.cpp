a00e6ee1933cb0e4bf20486ea163ecf1501bf8ba
miles <mtdaly@ncsu.edu>
2025-10-12 01:16:28 -0400

    Reading data from brain fully tested to work for data above max packet size, and multiple packets per libusb packet.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "common/SerialHandler.hpp"
 #include "common/packet/types/encoder.hpp"
+#include "common/packet/types/large.hpp"
 
 struct test_struct {
     int32_t a;
@@ -22,8 +23,15 @@ int main() {
         serial_handler.receive();
         if (std::optional<Packet> packet = serial_handler.pop_latest(PacketId::ENCODER))
         {
+            // Expected bytes for std::float64_t value of 67.69
+            // 5c 8f c2 f5 28 ec 50 40
             EncoderData data = packet->get_data<EncoderData>();
             printf("Data: %f\n", data.value);
+        }
+        else if (std::optional<Packet> packet = serial_handler.pop_latest(PacketId::LARGE))
+        {
+            LargeData data = packet->get_data<LargeData>();
+            printf("Data: %s\n", data.data);
         }
     }
 }
