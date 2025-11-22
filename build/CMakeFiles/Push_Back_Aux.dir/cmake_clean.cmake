dfae1bba023e6f5c44fb29ddb2ccd0970d6e6dc0
mtdaly <mtdaly@ncsu.edu>
2025-11-21 22:44:17 -0500

    Working test code

---
+++ build/CMakeFiles/Push_Back_Aux.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Push_Back_Aux.dir/common/Buffer.cpp.o"
+  "CMakeFiles/Push_Back_Aux.dir/common/Buffer.cpp.o.d"
+  "CMakeFiles/Push_Back_Aux.dir/common/SerialHandler.cpp.o"
+  "CMakeFiles/Push_Back_Aux.dir/common/SerialHandler.cpp.o.d"
+  "CMakeFiles/Push_Back_Aux.dir/common/packet/Packet.cpp.o"
+  "CMakeFiles/Push_Back_Aux.dir/common/packet/Packet.cpp.o.d"
+  "CMakeFiles/Push_Back_Aux.dir/common/utils.cpp.o"
+  "CMakeFiles/Push_Back_Aux.dir/common/utils.cpp.o.d"
+  "CMakeFiles/Push_Back_Aux.dir/main.cpp.o"
+  "CMakeFiles/Push_Back_Aux.dir/main.cpp.o.d"
+  "Push_Back_Aux"
+  "Push_Back_Aux.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Push_Back_Aux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Push_Back_Aux.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Push_Back_Aux.
+# This may be replaced when dependencies are built.
