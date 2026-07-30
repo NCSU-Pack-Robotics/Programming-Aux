2dd474cf60a4c503a3acbf9ffe54bef089fa2aa0
Koray <klatif@ncsu.edu>
2026-07-30 00:23:14 -0400

    Using UART for `PiPicoComm`

--- src/comms/PiPicoComm.cpp
+++ src/comms/PiPicoComm.cpp
@@ -1,41 +1,18 @@
 #include "PiPicoComm.hpp"
 
-#include <hardware/gpio.h>
-#include <hardware/uart.h>
-
-PiPicoComm::PiPicoComm() {
-    // Initialize UART
-    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_INSTANCE, UART_TX_PIN));
-    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_INSTANCE, UART_RX_PIN));
-
-    uart_init(UART_INSTANCE, UART_BAUDRATE);
-}
-
 PiPicoComm::~PiPicoComm() {
-    // Deinitialize UART
-    uart_deinit(UART_INSTANCE);
-
     mutex_exit(&mutex);
 }
 
-size_t PiPicoComm::read(unsigned char* buf, size_t count) {
+size_t PiPicoComm::read(unsigned char* buf, const size_t count) {
     if (count == 0) return 0;
 
-    // Block until at least 1 byte is available
-    uart_read_blocking(UART_INSTANCE, buf, 1);
-
-    size_t amt_read = 1;
-
-    // Read as many as are available, and don't go over count, mirroring the read syscall
-    while (amt_read < count && uart_is_readable(UART_INSTANCE)) {
-        buf[amt_read++] = uart_getc(UART_INSTANCE);
-    }
-
-    return amt_read;
+    const bool read_success = uart.read_bytes(buf, count);
+    return read_success ? count : 0;
 }
 
-void PiPicoComm::write(unsigned char* buf, size_t count) {
-    uart_write_blocking(UART_INSTANCE, buf, count);
+void PiPicoComm::write(unsigned char* buf, const size_t count) {
+    uart.send_bytes(buf, count);
 }
 
 void PiPicoComm::mutex_lock() {
--- src/comms/PiPicoComm.hpp
+++ src/comms/PiPicoComm.hpp
@@ -1,24 +1,25 @@
 #pragma once
 
-#include <hardware/uart.h>
 #include <sys/types.h>
-
 #include <pico/sync.h>
 
-
+#include "I2C.hpp"
+#include "Uart.hpp"
 #include "comms/AbstractComm.hpp"
+#include "config.hpp"
 
 /** Communication abstraction for the Raspberry Pi Pico 2 - rp2350A.
  * The pico communicates over UART */
 class PiPicoComm : public AbstractComm {
-    static constexpr uint UART_TX_PIN = 4;
-    static constexpr uint UART_RX_PIN = 5;
-    static constexpr uint UART_BAUDRATE = 115200;
-    static inline uart_inst_t* const UART_INSTANCE = uart1;
-
     mutex_t mutex{};
+
+    /** Uart instance to use for communicating with the brain. */
+    const Uart uart{Config::BRAIN_COMM::BAUD_RATE,
+                    Config::BRAIN_COMM::UART_ID,
+                    {Config::Pins::UART_TX_GPIO, Config::Pins::UART_RX_GPIO}};
+
 public:
-    PiPicoComm();
+    PiPicoComm() = default;
     ~PiPicoComm() override;
     size_t read(unsigned char* buf, size_t count) override;
     void write(unsigned char* buf, size_t count) override;
